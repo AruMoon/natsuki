31bb46efc1e2a45550e79378d4632f16961558d6
AruMoon <anton17082003@gmail.com>
2021-03-04 23:09:34 +0500

    Name of author

--- main.go
+++ main.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2021 Aru Moon (Anton Gubin)
+Copyright 2021 Aru Moon
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
