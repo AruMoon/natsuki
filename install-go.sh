2165b701230b548496964c3ccf1eaf3f9df7642a
AruMoon <anton17082003@gmail.com>
2021-03-04 22:08:33 +0500

    fuck environments

--- install-go.sh
+++ install-go.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 wget https://dl.google.com/go/go$GOVERSION.linux-amd64.tar.gz
 tar -zxvf go$GOVERSION.linux-amd64.tar.gz
-export GOPATH=$HOME/project/go
-export GOROOT=$GOPATH/bin
-export PATH=$GOROOT:$PATH
-echo $PATH
-echo $GOPATH
-echo $GOROOT
