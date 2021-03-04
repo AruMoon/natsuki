74cd3df8706031273fd5989747cfface3925b74b
AruMoon <anton17082003@gmail.com>
2021-03-04 18:16:29 +0500

    Comments

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2021 Aru Moon (Anton Gubin)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
 package natsuki
 
 import (
@@ -20,7 +36,7 @@ func Run(token string) *discordgo.Session {
 func HandleError(err error) error {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error occured: %v\n", err)
-		os.Exit(1)
+		os.Exit(1) // Comment this line to ignore errors
 		return err
 	}
 	return err
