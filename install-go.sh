d67de69f8cfe4914eebd910a634fb2eca92a39a2
AruMoon <anton17082003@gmail.com>
2021-03-04 21:39:24 +0500

    home path

--- install-go.sh
+++ install-go.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget https://dl.google.com/go/go$GOVERSION.linux-amd64.tar.gz
 tar -zxvf go$GOVERSION.linux-amd64.tar.gz
-export PATH=/project/go:$PATH
+export PATH=$HOME/project/go:$PATH
